d665e56d16aa0f0fecab8945f88e95878f62519c
miliiita <milagrosdemarchi9@gmail.com>
2025-09-30 19:30:22 -0300

    Cambio de controles para detectar joystick

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,10 +7,10 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     [Header("Controles (Inspector)")]
-    public KeyCode upKey = KeyCode.W;
-    public KeyCode downKey = KeyCode.S;
-    public KeyCode leftKey = KeyCode.A;
-    public KeyCode rightKey = KeyCode.D;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -25,10 +26,21 @@ public class PlayerController : MonoBehaviour
     {
         // Leemos input en Update (porque Input.* es frame-based)
         movement = Vector2.zero;
-        if (Input.GetKey(upKey))    movement.y += 1f;
-        if (Input.GetKey(downKey))  movement.y -= 1f;
-        if (Input.GetKey(leftKey))  movement.x -= 1f;
+        if (Input.GetKey(upKey)) movement.y += 1f;
+
+        //DETECTAR QUE JOYSTICK ES EL QUE SE ESTA USANDO DEPENDIENDO EL PERSONAJE
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            Debug.Log("Arriba");
+            movement.y += 1f;
+        }
+            
+        if (Input.GetKey(downKey)) movement.y -= 1f;
+        //if (Input.GetAxisRaw("Vertical") < 0) movement.y -= 1f;
+        if (Input.GetKey(leftKey)) movement.x -= 1f;
+        //if (Input.GetAxisRaw("Horizontal") < 0) movement.x -= 1f;
         if (Input.GetKey(rightKey)) movement.x += 1f;
+        //if (Input.GetAxisRaw("Horizontal") > 0) movement.x += 1f;
         movement = movement.normalized; // evita mover más rápido en diagonal
     }
 
